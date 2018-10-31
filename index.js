0c32acb211c549a1535146851fdf063a952aa0cb
Kevin <kevincxy96@gmail.com>
2018-10-31 17:25:05 +0800

    title and quote:

--- index.js
+++ index.js
@@ -8,7 +8,21 @@ bot.on('message', (msg) => {
 
   request({url: 'https://www.newcreation.org.sg/ajax/get/daily-devotional', 'headers': 'headers'}, function(error, response, body) {
     
-    console.log(body);
+
+
+    var split = body.split("h1");
+    var title = split[1].split("<p>")[1].split("<br>")[0];
+    split = split[2].split("<h4>");
+    split = split[1].split("</h4>");
+    var verse = split[0];
+    var quote = split[1];
+    
+    console.log(title);
+
+    // var verse = body.split("<h4>")[1].split("</h4>")[0];
+    console.log(verse);
+    console.log(quote);
+    
 
   })
   // fetch("https://www.newcreation.org.sg/ajax/get/daily-devotional").then(resp => resp.text()).then(text => console.log(text));
